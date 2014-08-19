fae72f29a8a97005ea4a0e73b2cb408107b1e7cc
Etai E <thevenhaim@Etais-MacBook-Pro.local>
2014-08-19 07:59:20 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -11,11 +11,23 @@
     [physicsNode addChild:character];
     
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    
+    //check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        //add a new obstacle
+        [self addObstacle];
+        
+        //Then reset the timer
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
